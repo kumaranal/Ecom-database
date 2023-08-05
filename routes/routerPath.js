7b1a91722339a089f5bc27e07870e796b6078245
kumaranal <analbiswas180@gmail.com>
2023-08-05 15:29:05 +0530

    second commit

--- routes/routerPath.js
+++ routes/routerPath.js
@@ -10,11 +10,11 @@ var passport=require('passport');
 require("../middlewares/passportConfig")
 router.post('/signup',bycriptfn,registrationfn)
 router.post('/login',login1fn)
-router.get('/tasks',passport.authenticate('jwt',{session:false}),getallfn)
-router.get('/tasks/:id',passport.authenticate('jwt',{session:false}),getspecificfn)
-router.post('/tasks',passport.authenticate('jwt',{session:false}),createfn)
-router.put('/tasks/:id',passport.authenticate('jwt',{session:false}),updatefn)
-router.delete('/tasks/:id',passport.authenticate('jwt',{session:false}),deletefn)
+router.get('/tasks',getallfn)
+router.get('/tasks/:id',getspecificfn)
+router.post('/tasks',createfn)
+router.put('/tasks/:id',updatefn)
+router.delete('/tasks/:id',deletefn)
 //  router.get('/locationsearch',passport.authenticate('jwt',{session:false}),locationsearch)
 
 module.exports=router;
